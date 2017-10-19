0c3c52841c2a7dea4f64fe57582a2b26c665a8a4
esutko <esutko@localhost.localdomain>
2017-10-19 13:36:08 -0400

    made JSON functions static

--- lib/ble_message/src/json.dart
+++ lib/ble_message/src/json.dart
@@ -2,11 +2,11 @@ library ble_message;
 
 
 abstract class JSON {
-	Map decode(String json_string) {
+	static Map decode(String json_string) {
 
 	}
 
-	String encode(Map json_map) {
+	static String encode(Map json_map) {
 		String json_string = "{\n";
 		for (String key in json_map.keys) {
 			json_string += "\t\"${key}\":${json_map[key]}\n";
